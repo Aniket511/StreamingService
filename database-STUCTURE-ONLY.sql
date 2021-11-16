4d71e7071852877cadd658204dd60308c2424ecc
Aniket511 <55677241+Aniket511@users.noreply.github.com>
2021-11-16 12:05:38 +0530

    Update database-STUCTURE-ONLY.sql

--- database-STUCTURE-ONLY.sql
+++ database-STUCTURE-ONLY.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Nov 17, 2019 at 03:19 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,20 +6,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `reeceflix`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `billingDetails`
---
 
 CREATE TABLE `billingDetails` (
   `id` int(11) NOT NULL,
